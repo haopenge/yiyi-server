762682a6e20e467b2fe1a612b53d47bf56d2978b
liupenghao <liupenghao@huohua.cn>
2021-12-23 15:33:40 +0800

    starter 项目 补充 example 测试接口

--- yiyi-examples/yiyi-example/src/main/java/com/yiyi/example/controller/UserController.java
+++ yiyi-examples/yiyi-example/src/main/java/com/yiyi/example/controller/UserController.java
@@ -1,12 +1,24 @@
 package com.yiyi.example.controller;
 
+import com.yi.auth.service.MsgService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
+
+    @Autowired
+    private MsgService msgService;
+
+    @PostMapping("/send_msg")
+    public String sendMsg(){
+        msgService.sendMessageOnXxPlatform("code_12138","16602223926","1","2","3");
+        return "send msg success";
+    }
 
     @GetMapping("/login")
     public String eatOrange() {
